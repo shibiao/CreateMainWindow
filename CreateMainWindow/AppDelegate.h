cc53a89a351caee69947794a9bb81edc62fa6216
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-12-21 10:08:10 +0800

    Initial Commit

---
+++ CreateMainWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CreateMainWindow
+//
+//  Created by sycf_ios on 2016/12/21.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
