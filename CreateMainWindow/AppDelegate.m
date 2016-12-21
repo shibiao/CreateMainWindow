1ef1deda67ee8129c579ca6f0753f87fcb122034
WalkWithWind <sycf_ios_13579@sycf-iosdeiMac.local>
2016-12-21 10:31:50 +0800

    create

--- CreateMainWindow/AppDelegate.m
+++ CreateMainWindow/AppDelegate.m
@@ -15,7 +15,20 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    //创建主window
+    NSWindow *mainWindow = [NSWindow new];
+    //设置主window大小
+    [mainWindow setFrame:CGRectMake(0, 0, 500, 500) display:YES];
+    mainWindow.collectionBehavior = NSWindowCollectionBehaviorFullScreenPrimary|NSWindowCollectionBehaviorDefault|NSWindowCollectionBehaviorManaged;
+    [mainWindow center];
+    mainWindow.styleMask = NSWindowStyleMaskTitled | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable | NSWindowStyleMaskClosable | NSWindowStyleMaskTexturedBackground;
+    [mainWindow becomeKeyWindow];
+    [NSApp beginModalSessionForWindow:mainWindow];
+    //添加contentView
+//    NSView *view = [[NSView alloc]initWithFrame:CGRectMake(0, 0, 500, 500)];
+//    view.wantsLayer = YES;
+//    view.layer.backgroundColor = [NSColor redColor].CGColor;
+//    mainWindow.contentView = view;
 }
 
 
